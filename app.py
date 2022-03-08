77fc344c523c6dd06fd5cf72d35c07b95f93428c
facu aquino <facun90@live.com>
2022-03-08 19:10:09 -0300

    basics python begginer

---
+++ app.py
@@ -0,0 +1,30 @@
+# basic comment in python
+print('hello world')
+
+
+print("")
+
+# variables in python
+# Python variables do not need explicit declaration to reserve memory space. The declaration happens automatically when you assign a value to a variable
+
+thing_name = 'man'
+a_number = 50
+print('there is a ' + thing_name + ' who is  a (?')
+
+# String Interpolation
+print(f'there is a {thing_name} who is  a (?')
+
+thing_name = 'car'
+
+print(f'now i am a new thing , i am a... {thing_name}')
+
+
+# varible types in python
+
+stringData = 'i am a string in python'
+number_data = 100
+booleanData = True
+
+
+if not booleanData & 0:
+    print('boolean ok')
